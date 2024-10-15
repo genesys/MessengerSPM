f181276a1f12f10015dbb12360b6cd173dd1c8cd
Golan Shoval Gil <golan.shoval_gil@genesys.com>
2024-10-15 17:26:29 +0300

    Attempting using the existing CoreSPM

---
+++ Sources/GenesysCloudMessengerWrapper/GenesysCloudMessengerWrapper.swift
@@ -0,0 +1,16 @@
+//
+//  GenesysCloudMessengerWrapper.swift
+//  GenesysCloudMessenger
+//
+//  Created by Golan Shoval Gil on 15/10/2024.
+//
+
+// Sources/GenesysCloudMessengerWrapper/GenesysCloudMessengerWrapper.swift
+
+import GenesysCloudCore
+import GenesysCloudMessenger
+import MessengerTransport
+
+public class GenesysCloudMessengerWrapper {
+    // This is a placeholder class. Implement your wrapper logic here.
+}
